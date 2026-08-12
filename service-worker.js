144f77e630627f2f4bd4ac5edff88f186aff2256
ardarawk-cloud <ardarawk@gmail.com>
2026-08-13 02:34:27 +0800

    BUILD256 refresh PWA cache identity

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="acc-os-x-build-253-kai-creative-engine";
-const CORE=["./","./index.html","./app.js","./home-launchpad.js","./poster-studio-v1.js?rev=BUILD253_KAI_CREATIVE","./kai-creative-client.js?rev=BUILD253_KAI_CREATIVE_CLIENT_V1","./manifest.webmanifest","./acc-os-x-192-build250.png","./acc-os-x-512-build250.png","./acc-os-x-maskable-512-build250.png","./version.json"];
+const CACHE="acc-os-x-build-256-release-sync";
+const CORE=["./","./index.html","./app.js","./home-launchpad.js","./release-version-v256.js?rev=BUILD256_RELEASE_SYNC","./poster-studio-v1.js?rev=BUILD253_KAI_CREATIVE","./kai-creative-client.js?rev=BUILD253_KAI_CREATIVE_CLIENT_V1","./pwa-owner-safety-v1.js?rev=PWA_OWNER_SAFETY_V1","./manifest.webmanifest","./acc-os-x-192-build250.png","./acc-os-x-512-build250.png","./acc-os-x-maskable-512-build250.png","./version.json","./build-info.json","./build.json"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
