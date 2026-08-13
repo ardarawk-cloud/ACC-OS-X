ee41a38ac65fb00414a800e7cdbe2a4a5f97a3fa
ardarawk-cloud <ardarawk@gmail.com>
2026-08-14 01:46:16 +0800

    BUILD258.1 add release loader marker

---
+++ release-loader-v2581.js
@@ -0,0 +1,3 @@
+// ACC OS X — BUILD 258.1 release-loader marker.
+// The active release identity is provided by release-version-v258.js.
+window.ACCReleaseLoaderV2581={revision:"BUILD258_1_CONTRACT_HARDENING"};
